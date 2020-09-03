13d2586616743bf39130aa97da33b0e2dc64ac31
kotaesaki <gurico1229@gmail.com>
2020-09-03 16:10:02 +0900

    test

---
+++ face_api.py
@@ -0,0 +1,81 @@
+import asyncio
+import io
+import glob
+import os
+import sys
+import time
+import uuid
+import requests
+import json
+import urllib3
+from urllib.parse import urlparse
+from io import BytesIO
+from PIL import Image, ImageDraw
+from azure.cognitiveservices.vision.face import FaceClient
+from msrest.authentication import CognitiveServicesCredentials
+from azure.cognitiveservices.vision.face.models import TrainingStatusType, Person, SnapshotObjectType, OperationStatusType
+import cognitive_face as CF
+
+
+class FaceApi(object):
+
+	#サブスクリプションキーの設定
+	KEY = 'a3f5affaebb549449f69ccd3106d3e75'
+
+	#エンドポイント設定
+	ENDPOINT = 'https://japaneast.api.cognitive.microsoft.com/face/v1.0'
+
+	#画像のurl
+	image_url = 'IMG_2179.jpeg'
+
+	CF.Key.set(KEY)
+	CF.BaseUrl.set(ENDPOINT)
+
+	faces = CF.face.detect(image_url, attributes='emotion')
+
+	print(faces)
+
+	
+
+
+
+
+'''
+# Create an authenticated FaceClient.
+face_client = FaceClient(ENDPOINT, CognitiveServicesCredentials(KEY))
+
+headers = {'Ocp-Apim-Subscription-Key': KEY}
+
+params = {
+    'returnFaceId': 'true',
+    'returnFaceLandmarks': 'false',
+    'returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise',
+}
+
+#JSON出力
+try:
+	response = requests.post(ENDPOINT, params=params,
+                         headers=headers, json={"url": image_url}, timeout=0.1)
+	print(json.dumps(response.json()))
+
+except requests.exceptions.ConnectTimeout:
+	print('エラー！')
+
+
+
+
+single_image_name = os.path.basename(single_face_image_url)
+detected_faces = face_client.face.detect_with_url(url=single_face_image_url)
+if not detected_faces:
+    raise Exception('No face detected from image {}'.format(single_image_name))
+
+# Display the detected face ID in the first single-face image.
+# Face IDs are used for comparison to faces (their IDs) detected in other images.
+print('Detected face ID from', single_image_name, ':')
+for face in detected_faces: print (face.face_id)
+print()
+
+# Save this ID for use in Find Similar
+first_image_face_ID = detected_faces[0].face_id
+
+'''
