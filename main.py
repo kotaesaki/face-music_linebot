8525cef28e05faf70ebf579af289c5d740607f95
kotaesaki <gurico1229@gmail.com>
2020-09-03 16:13:31 +0900

    test

--- main.py
+++ main.py
@@ -46,11 +46,12 @@ def callback():
 #以下でWebhookから送られてきたイベントをどのように処理するかを記述する
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    '''
     if event.message.type == image:
         line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text="画像だね"))
-        
+    '''    
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
