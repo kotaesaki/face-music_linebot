ba056cf3f79e2b02ec3393e5b4a18de5f595a5e2
kotaesaki <gurico1229@gmail.com>
2020-09-03 20:52:16 +0900

    test

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ def handle_message(event):
     print(message_id)
 
     #face_api.pyで画像分析
-    face[] = face_api.FaceApi(getImageLine(message_id))
+    face{} = face_api.FaceApi(getImageLine(message_id))
 
     line_bot_api.reply_message(
     event.reply_token,
